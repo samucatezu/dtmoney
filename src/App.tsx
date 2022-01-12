c1d3c8df21f4d90916b4f35ca41b998c90a94818
Samuel Almeida <barbosasamuel24@gmail.com>
2022-01-12 18:13:40 -0300

    Global styles created

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
       <title>Hello World</title>
+      <GlobalStyle />
     </div>
   );
 }
