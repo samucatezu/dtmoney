1590279eed38a2cb7e756ca3ab555aaf752c11e7
Samuel Almeida <barbosasamuel24@gmail.com>
2022-01-16 16:45:39 -0300

    Seeds and transactions listed

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,96 +1,47 @@
-import { FormEvent, useState } from "react";
-import Modal from "react-modal";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
+import { Container } from "./styles";
 
-import closeImg from "../../assets/close.svg";
-import incomeImg from "../../assets/income.svg";
-import outcomeImg from "../../assets/outcome.svg";
-
-import { Container, TransactionTypeContainer, RadioBox } from "./styles";
-
-interface NewTransactionModalProps {
-  isOpen: boolean;
-  onRequestClose: () => void;
+interface Transaction {
+  id: number;
+  title: string;
+  type: string;
+  category: string;
+  amount: number;
+  createdAt: string;
 }
 
-export const NewTransactionModal = ({
-  isOpen,
-  onRequestClose,
-}: NewTransactionModalProps) => {
-  const [type, setType] = useState("deposit");
-  const [title, setTitle] = useState("");
-  const [amount, setAmout] = useState(0);
-  const [category, setCategory] = useState("");
+export function TransactionsTable() {
+  const [transactions, setTransaction] = useState<Transaction[]>([]);
 
-  function handleCreateNewTransaction(event: FormEvent) {
-    event.preventDefault();
-    const data = { type, title, amount, category };
-    api.post("/transactions", data);
-  }
+  useEffect(() => {
+    api
+      .get("/transactions")
+      .then((response) => setTransaction(response.data.transactions));
+  }, []);
 
   return (
-    <Modal
-      isOpen={isOpen}
-      onRequestClose={onRequestClose}
-      overlayClassName="react-modal-overlay"
-      className="react-modal-content"
-    >
-      <button
-        className="react-modal-close"
-        type="button"
-        onClick={onRequestClose}
-      >
-        <img src={closeImg} alt="Fechal modal" />
-      </button>
-
-      <Container onSubmit={handleCreateNewTransaction}>
-        <h2>Cadastrar Transação</h2>
-
-        <input
-          type="text"
-          placeholder="Título"
-          value={title}
-          onChange={({ target }) => setTitle(target.value)}
-        />
-
-        <input
-          type="number"
-          placeholder="Valor"
-          value={amount}
-          onChange={({ target }) => setAmout(Number(target.value))}
-        />
-
-        <TransactionTypeContainer>
-          <RadioBox
-            type="button"
-            onClick={() => setType("deposit")}
-            isActive={type === "deposit"}
-            activeColor="green"
-          >
-            <img src={incomeImg} alt="Entrada" />
-            <span>Entrada</span>
-          </RadioBox>
-
-          <RadioBox
-            type="button"
-            onClick={() => setType("withdraw")}
-            isActive={type === "withdraw"}
-            activeColor="red"
-          >
-            <img src={outcomeImg} alt="Saída" />
-            <span>Saída</span>
-          </RadioBox>
-        </TransactionTypeContainer>
-
-        <input
-          type="text"
-          placeholder="Categoria"
-          value={category}
-          onChange={({ target }) => setCategory(target.value)}
-        />
-
-        <button type="submit">Cadastrar</button>
-      </Container>
-    </Modal>
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>{transaction.amount}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.createdAt}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Container>
   );
-};
+}
--- src/index.tsx
+++ src/index.tsx
@@ -7,15 +7,40 @@ createServer({
   models: {
     transaction: Model,
   },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelance de website",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1100,
+          createdAt: new Date("2021-02-12 16:00:00"),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = "api";
 
     this.get("/transactions", () => {
-      return this.schema.all("transactions");
+      return this.schema.all("transaction");
     });
 
     this.post("/transactions", (schema, request) => {
       const data = JSON.parse(request.requestBody);
+
       return schema.create("transaction", data);
     });
   },
