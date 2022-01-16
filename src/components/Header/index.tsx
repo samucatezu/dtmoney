634b567ed27645797af1310a2fd090202c7a5c28
Samuel Almeida <barbosasamuel24@gmail.com>
2022-01-16 00:04:31 -0300

    Config create modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,18 @@
 import logoImg from "../../assets/logo.svg";
+
 import { Container, Content } from "./styles";
 
-export function Header() {
-  return (
-    <Container>
-      <Content>
-        <img src={logoImg} alt="dtmoney" />
-        <button type="button">Nova transação</button>
-      </Content>
-    </Container>
-  );
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
 }
+
+export const Header = ({ onOpenNewTransactionModal }: HeaderProps) => (
+  <Container>
+    <Content>
+      <img src={logoImg} alt="dt money" />
+      <button type="button" onClick={onOpenNewTransactionModal}>
+        Nova Transação
+      </button>
+    </Content>
+  </Container>
+);
